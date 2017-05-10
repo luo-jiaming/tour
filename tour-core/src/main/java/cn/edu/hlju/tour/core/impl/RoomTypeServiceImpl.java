1077bffbc63bc00eaa49b0d0cfe20a5b5ba02e3b
Sole <2606646771@qq.com>
2017-05-10 23:56:35 +0800

    更新

--- tour-core/src/main/java/cn/edu/hlju/tour/core/impl/RoomTypeServiceImpl.java
+++ tour-core/src/main/java/cn/edu/hlju/tour/core/impl/RoomTypeServiceImpl.java
@@ -16,8 +16,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Sole on 2017/4/10.
@@ -50,20 +52,9 @@ public class RoomTypeServiceImpl implements RoomTypeService {
     @Override
     public void update(RoomType roomType) {
         roomTypeMapper.updateByPrimaryKeySelective(roomType);
+        //更新酒店最低价格
         Long hotelId = roomType.getHotelId();
-        //更改酒店最低价格
-        int low = Integer.MAX_VALUE;
-        List<RoomType> list = roomTypeMapper.selectByHotelId(hotelId);
-        for (RoomType type : list) {
-            int price = Integer.parseInt(type.getPrice());
-            if (price < low) {
-                low = price;
-            }
-        }
-        Hotel hotel = new Hotel();
-        hotel.setId(hotelId);
-        hotel.setPrice(Integer.toString(low));
-        hotelMapper.updateByPrimaryKeySelective(hotel);
+        setLowerPrice(hotelId);
     }
 
     @Override
@@ -74,12 +65,44 @@ public class RoomTypeServiceImpl implements RoomTypeService {
 
     @Override
     public void delRoomType(Long[] ids) {
+        Set<Long> hotelIds = new HashSet();
+        for (Long id : ids) {
+            RoomType roomType = roomTypeMapper.selectByPrimaryKey(id);
+            hotelIds.add(roomType.getHotelId());
+        }
+        //删除酒店房间类型
         roomTypeMapper.delByRoomTypeId(ids);
+        //更新hotelIds的酒店最低价格
+        for (Long id : hotelIds) {
+            setLowerPrice(id);
+        }
     }
 
     @Override
     public void addRoomType(RoomType roomType) {
         roomTypeMapper.insertSelective(roomType);
+        //更新酒店最低价格
+        Long hotelId = roomType.getHotelId();
+        setLowerPrice(hotelId);
+    }
+
+    private void setLowerPrice(Long  hotelId) {
+        //更改酒店最低价格
+        int low = Integer.MAX_VALUE;
+        List<RoomType> list = roomTypeMapper.selectByHotelId(hotelId);
+        for (RoomType type : list) {
+            int price = Integer.parseInt(type.getPrice());
+            if (price < low) {
+                low = price;
+            }
+        }
+        if (low == Integer.MAX_VALUE) {
+            low = 0;
+        }
+        Hotel hotel = new Hotel();
+        hotel.setId(hotelId);
+        hotel.setPrice(Integer.toString(low));
+        hotelMapper.updateByPrimaryKeySelective(hotel);
     }
 
 }
